2cd3a40d24b865d222dc50e1df0e8ff904dd1bdd
pierre.matthey <pierre.matthey@heig-vd.com>
2025-10-09 11:58:24 +0200

    commit ca marche pas 2

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import PouchDB from 'pouchdb'
 const counter = ref(110)
 const increment = () => {
   counter.value++
 }
 </script>
-
 <template>
   <h1>Welcome</h1>
   <p>Counter: {{ counter }}</p>
