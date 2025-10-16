34599755bbc9c9e5db6f077a426b4ff247da8e91
pierre.matthey <pierre.matthey@heig-vd.com>
2025-10-16 10:55:56 +0200

    TheWelcome.vues fetch doesn't work

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,13 +1,68 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import PouchDB from 'pouchdb'
-const counter = ref(110)
-const increment = () => {
-  counter.value++
+
+declare interface Post {
+  _id: string
+  _rev: string
+  type: string
+  name: {
+    first: string
+    last: string
+  }
+  email: string
+  tags: string[]
+  created_at: string // ou Date
 }
+
+// Référence à la base de données
+const storage = ref()
+// Données stockées
+const postsData = ref<Post[]>([])
+
+// Initialisation de la base de données
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données')
+  const db = new PouchDB('http://admin:admin@127.0.0.1:5986/test_database')
+  if (db) {
+    console.log('Connecté à la collection : ' + db?.name)
+    storage.value = db
+  } else {
+    console.warn('Echec lors de la connexion à la base de données')
+  }
+}
+
+// Récupération des données
+const fetchData = () => {
+  if (!storage.value) {
+    console.warn('Base de données non initialisée')
+    return
+  }
+  storage.value
+    .allDocs({ include_docs: true })
+    .then((result) => {
+      console.log(result)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+onMounted(() => {
+  console.log('=> Composant initialisé')
+  initDatabase()
+  //fetchData()
+})
 </script>
+
 <template>
-  <h1>Welcome</h1>
-  <p>Counter: {{ counter }}</p>
-  <button @click="increment">Increment</button>
+  <h1>Fetch Data</h1>
+  <article v-for="post in postsData" v-bind:key="(post as any).id">
+    <h2>{{ post.name.first }}</h2>
+    <p>{{ post.name.last }}</p>
+    <p>{{ post.email }}</p>
+    <p>{{ post.tags.join(', ') }}</p>
+    <p>{{ post.created_at }}</p>
+  </article>
+  <button role="button" @click="fetchData">Fetch</button>
 </template>
